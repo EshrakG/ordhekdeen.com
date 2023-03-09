0b143086d43fba69b140a3ef95b097f3e522e996
eshrakg <eshrakg62@gmail.com>
2023-03-10 00:52:15 +0600

    first commit

---
+++ meltrimonial3/index.js
@@ -0,0 +1,23 @@
+const home = ()=>{
+    window.location.assign("index.html")
+}
+const aboutUs = ()=>{
+    window.location.assign("aboutUs.html")
+}
+
+
+const questons = ()=>{
+    window.location.assign("questions.html")
+}
+
+const tips = ()=>{
+    window.location.assign("tips.html")
+}
+
+const contact = ()=>{
+    window.location.assign("contact.html")
+}
+
+const login = () =>{
+    window.location.assign("login.html")
+}
